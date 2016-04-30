a092a892edad512bebb62e704069f86fd24b8e93
Jonathan <pkne1753@uni.sydney.edu.au>
2016-04-30 20:53:01 +1000

    added signature for master and verifying for bot

--- botMaster/master_sign.py
+++ botMaster/master_sign.py
@@ -1,31 +1,53 @@
 import os
-from botMaster.SigningHelper import SigningHelper
+from botMaster.RSASignVerify import RSASignAndVerify
+
 
 def sign_file(f):
-    signingHelper = SigningHelper()
-    signature = signingHelper.sign(f)
-    print("Message has been signed result is {}".format(signature))
-    signingHelper.savePubKey()
-    return signature
+    master_signature = RSASignAndVerify().sign_file(f)
+    new_line = bytes('\n', "ascii")
+    end_signature = bytes('END', "ascii")
+    return master_signature + end_signature + new_line + f
 
-def saveFile(filepath,file):
-    signed_fn = os.path.join("../pastebot.net", filepath + ".signed")
-    out = open(signed_fn, "wb")
-    out.write(file)
-    out.close()
 
 if __name__ == "__main__":
-    fn = input("Which file in pastebot.net should be signed? ")
-    if not os.path.exists(os.path.join("../pastebot.net", fn)):
-        print("The given file doesn't exist on pastebot.net")
-        os.exit(1)
-    f = open(os.path.join("../pastebot.net", fn), "rb").read()
-    saveFile(fn,f)
-    signed_f = sign_file(f)
-    out = open("../Signatures/signature", "wb")
-    out.write(signed_f)
-    out.close()
-    # print("Signed file written to", out.)
+    if __name__ == "__main__":
+        fn = input("Which file in pastebot.net should be signed? ")
+        if not os.path.exists(os.path.join("../pastebot.net", fn)):
+            print("The given file doesn't exist on pastebot.net")
+            os.exit(1)
+        f = open(os.path.join("../pastebot.net", fn), "rb").read()
+        signed_f = sign_file(f)
+        signed_fn = os.path.join("../pastebot.net", fn + ".signed")
+        out = open(signed_fn, "wb")
+        out.write(signed_f)
+        out.close()
+
+        signed_file = open('../pastebot.net/hello.fbi.signed','rb')
+
+        signature = b''
+        content = b''
+        delimiterFound = False
+        for line in signed_file:
+
+            if line == b'\n':
+                delimiterFound = True
+                continue
+
+            if delimiterFound:
+                content += line
+
+            if not delimiterFound:
+                signature += line
+
+        print("signature is {}".format(signature))
+        print("content is {}".format(content))
+
+
+
+        signed_file.close()
+
+
+        print("Signed file written to", signed_fn)
 
 # Bot master generates a public/private key pair
 # Bot master signs the message to be sent with its own private key
